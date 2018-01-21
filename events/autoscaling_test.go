9873619e3654e0d0f437857cb5e64c9bca60838b
Gaurav Mehta <gaurav.mehta@iag.com.au>
2018-01-21 18:06:02 +1100

    Added additional test files. Changed some field to pointers to ignore them during encoding when the fields are empty.

--- events/autoscaling_test.go
+++ events/autoscaling_test.go
@@ -8,23 +8,30 @@ import (
 
 func TestAutoScalingEventMarshaling(t *testing.T) {
 
-	// 1. read JSON from file
-	inputJson := readJsonFromFile(t, "./testdata/autoscaling-event.json")
+	var sampleFileList = []string{"autoscaling-event-launch-successful.json", "autoscaling-event-launch-unsuccessful.json", "autoscaling-event-lifecycle-action.json",
+		"autoscaling-event-terminate-action.json", "autoscaling-event-terminate-successful.json", "autoscaling-event-terminate-unsuccessful.json"}
 
-	// 2. de-serialize into Go object
-	var inputEvent AutoScalingEvent
-	if err := json.Unmarshal(inputJson, &inputEvent); err != nil {
-		t.Errorf("could not unmarshal event. details: %v", err)
+	// Loop over list and test each file individually //
+	for _, sampleFile := range sampleFileList {
+
+		t.Logf("Running test for %s\n", sampleFile)
+		// 1. read JSON from file
+		inputJson := readJsonFromFile(t, "./testdata/"+sampleFile)
+
+		// 2. de-serialize into Go object
+		var inputEvent AutoScalingEvent
+		if err := json.Unmarshal(inputJson, &inputEvent); err != nil {
+			t.Errorf("could not unmarshal event. details: %v", err)
+		}
+		// 3. serialize to JSON
+		outputJson, err := json.Marshal(inputEvent)
+		if err != nil {
+			t.Errorf("could not marshal event. details: %v", err)
+		}
+		// 4. check result
+		test.AssertJsonsEqual(t, inputJson, outputJson)
 	}
 
-	// 3. serialize to JSON
-	outputJson, err := json.Marshal(inputEvent)
-	if err != nil {
-		t.Errorf("could not marshal event. details: %v", err)
-	}
-
-	// 4. check result
-	test.AssertJsonsEqual(t, inputJson, outputJson)
 }
 
 func TestAutoScalingMarshalingMalformedJson(t *testing.T) {
