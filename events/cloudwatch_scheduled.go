92c29ae63f1b65360c4069a962cd0decad2350df
Jon Bristow <jon.bristow@teradata.com>
2018-01-25 12:15:20 -0800

    Adding events/cloudwatch_scheduled

---
+++ events/cloudwatch_scheduled.go
@@ -0,0 +1,19 @@
+package events
+
+import (
+	"encoding/json"
+	"time"
+)
+
+//CloudwatchScheduledEvent captures event data sent from a CloudWatch schedule trigger.
+type CloudwatchScheduledEvent struct {
+	Version    string          `json:"version"`
+	ID         string          `json:"id"`
+	DetailType string          `json:"detail-type"`
+	Source     string          `json:"source"`
+	AccountID  string          `json:"account"`
+	Time       time.Time       `json:"time"`
+	Region     string          `json:"region"`
+	Resources  []string        `json:"resources"`
+	Detail     json.RawMessage `json:"detail"`
+}
