fa1a118928cc98dce34b990f0ab244dc13b1a8e6
willnewrelic <will@newrelic.com>
2018-10-26 11:20:34 -0700

    Make newHandler public (#134)

--- lambda/handler.go
+++ lambda/handler.go
@@ -70,14 +70,17 @@ func validateReturns(handler reflect.Type) error {
 	return nil
 }
 
-// newHandler Creates the base lambda handler, which will do basic payload unmarshaling before defering to handlerSymbol.
-// If handlerSymbol is not a valid handler, the returned function will be a handler that just reports the validation error.
-func newHandler(handlerSymbol interface{}) lambdaHandler {
-	if handlerSymbol == nil {
+// NewHandler creates a base lambda handler from the given handler function. The
+// returned Handler performs JSON deserialization and deserialization, and
+// delegates to the input handler function.  The handler function parameter must
+// satisfy the rules documented by Start.  If handlerFunc is not a valid
+// handler, the returned Handler simply reports the validation error.
+func NewHandler(handlerFunc interface{}) Handler {
+	if handlerFunc == nil {
 		return errorHandler(fmt.Errorf("handler is nil"))
 	}
-	handler := reflect.ValueOf(handlerSymbol)
-	handlerType := reflect.TypeOf(handlerSymbol)
+	handler := reflect.ValueOf(handlerFunc)
+	handlerType := reflect.TypeOf(handlerFunc)
 	if handlerType.Kind() != reflect.Func {
 		return errorHandler(fmt.Errorf("handler kind %s is not %s", handlerType.Kind(), reflect.Func))
 	}
@@ -91,7 +94,7 @@ func newHandler(handlerSymbol interface{}) lambdaHandler {
 		return errorHandler(err)
 	}
 
-	return func(ctx context.Context, payload []byte) (interface{}, error) {
+	return lambdaHandler(func(ctx context.Context, payload []byte) (interface{}, error) {
 		// construct arguments
 		var args []reflect.Value
 		if takesContext {
@@ -123,5 +126,5 @@ func newHandler(handlerSymbol interface{}) lambdaHandler {
 		}
 
 		return val, err
-	}
+	})
 }
