141c11e49a77165705813f8b5e55cd24443da1f5
Sjur Fredriksen <sjur.fredriksen@get.no>
2018-12-14 13:06:22 +0100

    Add tests and support for MultiValueHeaders and MultiValueQueryStringParameters

--- events/alb.go
+++ events/alb.go
@@ -2,15 +2,15 @@ package events
 
 // LambdaTargetGroupRequest contains data originating from the ALB Lambda target group integration
 type LambdaTargetGroupRequest struct {
-	HTTPMethod            string                          `json:"httpMethod"`
-	Path                  string                          `json:"path"`
-	QueryStringParameters map[string]string               `json:"queryStringParameters"`
-	Headers               map[string]string               `json:"headers"`
-	PathParameters        map[string]string               `json:"pathParameters"`
-	StageVariables        map[string]string               `json:"stageVariables"`
-	RequestContext        LambdaTargetGroupRequestContext `json:"requestContext"`
-	IsBase64Encoded       bool                            `json:"isBase64Encoded,omitempty"`
-	Body                  string                          `json:"body"`
+	HTTPMethod                      string                          `json:"httpMethod"`
+	Path                            string                          `json:"path"`
+	QueryStringParameters           map[string]string               `json:"queryStringParameters"`
+	MultiValueQueryStringParameters map[string][]string             `json:"multiValueQueryStringParameters"`
+	Headers                         map[string]string               `json:"headers"`
+	MultiValueHeaders               map[string][]string             `json:"multiValueHeaders"`
+	RequestContext                  LambdaTargetGroupRequestContext `json:"requestContext"`
+	IsBase64Encoded                 bool                            `json:"isBase64Encoded"`
+	Body                            string                          `json:"body"`
 }
 
 // LambdaTargetGroupRequestContext contains the information to identify the load balancer invoking the lambda
@@ -25,9 +25,10 @@ type ELBContext struct {
 
 // LambdaTargetGroupResponse configures the response to be returned by the ALB Lambda target group for the request
 type LambdaTargetGroupResponse struct {
-	StatusCode        int               `json:"statusCode"`
-	StatusDescription string            `json:"statusDescription"`
-	Headers           map[string]string `json:"headers"`
-	Body              string            `json:"body"`
-	IsBase64Encoded   bool              `json:"isBase64Encoded"`
+	StatusCode        int                 `json:"statusCode"`
+	StatusDescription string              `json:"statusDescription"`
+	Headers           map[string]string   `json:"headers"`
+	MultiValueHeaders map[string][]string `json:"multiValueHeaders"`
+	Body              string              `json:"body"`
+	IsBase64Encoded   bool                `json:"isBase64Encoded"`
 }
---
+++ events/alb_test.go
@@ -0,0 +1,59 @@
+package events
+
+import (
+	"testing"
+	"io/ioutil"
+	"encoding/json"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/aws/aws-lambda-go/events/test"
+)
+
+func TestAlbLambdaTargetRequestMarshaling(t *testing.T) {
+	// read json from file
+	inputJSON, err := ioutil.ReadFile("./testdata/alb-lambda-target-request.json")
+	if err != nil {
+		t.Errorf("could not open test file. details: %v", err)
+	}
+
+	// de-serialize into Go object
+	var inputEvent LambdaTargetGroupRequest
+	if err := json.Unmarshal(inputJSON, &inputEvent); err != nil {
+		t.Errorf("could not unmarshal event. details: %v", err)
+	}
+
+	// serialize to json
+	outputJSON, err := json.Marshal(inputEvent)
+	if err != nil {
+		t.Errorf("could not marshal event. details: %v", err)
+	}
+
+	assert.JSONEq(t, string(inputJSON), string(outputJSON))
+}
+
+func TestAlbLambdaTargetRequestMalformedJson(t *testing.T) {
+	test.TestMalformedJson(t, LambdaTargetGroupRequest{})
+}
+
+func TestAlbLambdaTargetResponseMarshaling(t *testing.T) {
+
+	// read json from file
+	inputJSON, err := ioutil.ReadFile("./testdata/alb-lambda-target-response.json")
+	if err != nil {
+		t.Errorf("could not open test file. details: %v", err)
+	}
+
+	// de-serialize into Go object
+	var inputEvent LambdaTargetGroupResponse
+	if err := json.Unmarshal(inputJSON, &inputEvent); err != nil {
+		t.Errorf("could not unmarshal event. details: %v", err)
+	}
+
+	// serialize to json
+	outputJSON, err := json.Marshal(inputEvent)
+	if err != nil {
+		t.Errorf("could not marshal event. details: %v", err)
+	}
+
+	assert.JSONEq(t, string(inputJSON), string(outputJSON))
+}
