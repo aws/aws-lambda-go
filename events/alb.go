26cdb2eaa3e64f93908b8b7a0ca8d07df54ee276
Bryan Moffatt <bmoffatt@users.noreply.github.com>
2018-12-14 18:52:31 +0000

    Update alb.go
    Changed the type time. s/Lambda/ALB/
    updated field name casing for ALBTargetGroupRequestContext.ELB

--- events/alb.go
+++ events/alb.go
@@ -1,21 +1,21 @@
 package events
 
-// LambdaTargetGroupRequest contains data originating from the ALB Lambda target group integration
-type LambdaTargetGroupRequest struct {
-	HTTPMethod                      string                          `json:"httpMethod"`
-	Path                            string                          `json:"path"`
-	QueryStringParameters           map[string]string               `json:"queryStringParameters"`
-	MultiValueQueryStringParameters map[string][]string             `json:"multiValueQueryStringParameters"`
-	Headers                         map[string]string               `json:"headers"`
-	MultiValueHeaders               map[string][]string             `json:"multiValueHeaders"`
-	RequestContext                  LambdaTargetGroupRequestContext `json:"requestContext"`
-	IsBase64Encoded                 bool                            `json:"isBase64Encoded"`
-	Body                            string                          `json:"body"`
+// ALBTargetGroupRequest contains data originating from the ALB Lambda target group integration
+type ALBTargetGroupRequest struct {
+	HTTPMethod                      string                       `json:"httpMethod"`
+	Path                            string                       `json:"path"`
+	QueryStringParameters           map[string]string            `json:"queryStringParameters"`
+	MultiValueQueryStringParameters map[string][]string          `json:"multiValueQueryStringParameters"`
+	Headers                         map[string]string            `json:"headers"`
+	MultiValueHeaders               map[string][]string          `json:"multiValueHeaders"`
+	RequestContext                  ALBTargetGroupRequestContext `json:"requestContext"`
+	IsBase64Encoded                 bool                         `json:"isBase64Encoded"`
+	Body                            string                       `json:"body"`
 }
 
-// LambdaTargetGroupRequestContext contains the information to identify the load balancer invoking the lambda
-type LambdaTargetGroupRequestContext struct {
-	Elb ELBContext `json:"elb"`
+// ALBTargetGroupRequestContext contains the information to identify the load balancer invoking the lambda
+type ALBTargetGroupRequestContext struct {
+	ELB ELBContext `json:"elb"`
 }
 
 // ELBContext contains the information to identify the ARN invoking the lambda
@@ -23,8 +23,8 @@ type ELBContext struct {
 	TargetGroupArn string `json:"targetGroupArn"`
 }
 
-// LambdaTargetGroupResponse configures the response to be returned by the ALB Lambda target group for the request
-type LambdaTargetGroupResponse struct {
+// ALBTargetGroupResponse configures the response to be returned by the ALB Lambda target group for the request
+type ALBTargetGroupResponse struct {
 	StatusCode        int                 `json:"statusCode"`
 	StatusDescription string              `json:"statusDescription"`
 	Headers           map[string]string   `json:"headers"`
