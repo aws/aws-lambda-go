18bf34bcbc333056a48b159118b1ccce0618ebb0
Bhavik Kumar <bhavikk85@gmail.com>
2019-05-10 00:29:27 +1200

    Close gzip reader as per documentation (#187)

--- events/cloudwatch_logs.go
+++ events/cloudwatch_logs.go
@@ -26,6 +26,7 @@ func (c CloudwatchLogsRawData) Parse() (d CloudwatchLogsData, err error) {
 	}
 
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
+	defer zr.Close()
 	if err != nil {
 		return
 	}
