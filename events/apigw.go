d851c22603ad2307124138811bd9829e39708e24
Roberto Duessmann <roberto.duessmann@gmail.com>
2019-10-22 18:11:17 +0200

    Fix typo regarding IAMPolicyStatement

--- events/apigw.go
+++ events/apigw.go
@@ -168,7 +168,7 @@ type APIGatewayCustomAuthorizerPolicy struct {
 	Statement []IAMPolicyStatement
 }
 
-// IAMPolicyStatement represents one statment from IAM policy with action, effect and resource
+// IAMPolicyStatement represents one statement from IAM policy with action, effect and resource
 type IAMPolicyStatement struct {
 	Action   []string
 	Effect   string
