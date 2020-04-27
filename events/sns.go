fdfee45301385ba3f4ad62cd8031e60a64574130
Mike Breen <hardbap@gmail.com>
2020-04-27 17:12:12 -0400

    Add SNSMessageAttribute

--- events/sns.go
+++ events/sns.go
@@ -18,15 +18,20 @@ type SNSEventRecord struct {
 }
 
 type SNSEntity struct {
-	Signature         string                 `json:"Signature"`
-	MessageID         string                 `json:"MessageId"`
-	Type              string                 `json:"Type"`
-	TopicArn          string                 `json:"TopicArn"`
-	MessageAttributes map[string]interface{} `json:"MessageAttributes"`
-	SignatureVersion  string                 `json:"SignatureVersion"`
-	Timestamp         time.Time              `json:"Timestamp"`
-	SigningCertURL    string                 `json:"SigningCertUrl"`
-	Message           string                 `json:"Message"`
-	UnsubscribeURL    string                 `json:"UnsubscribeUrl"`
-	Subject           string                 `json:"Subject"`
+	Signature         string                         `json:"Signature"`
+	MessageID         string                         `json:"MessageId"`
+	Type              string                         `json:"Type"`
+	TopicArn          string                         `json:"TopicArn"`
+	MessageAttributes map[string]SNSMessageAttribute `json:"MessageAttributes"`
+	SignatureVersion  string                         `json:"SignatureVersion"`
+	Timestamp         time.Time                      `json:"Timestamp"`
+	SigningCertURL    string                         `json:"SigningCertUrl"`
+	Message           string                         `json:"Message"`
+	UnsubscribeURL    string                         `json:"UnsubscribeUrl"`
+	Subject           string                         `json:"Subject"`
+}
+
+type SNSMessageAttribute struct {
+	Type  string `json:"Type"`
+	Value string `json:"Value"`
 }
