26aa36445a99488623e4b21c0e5c0b4dc684eff9
Chris Reeves <hello@chris.reeves.io>
2020-05-30 02:27:16 +0100

    Support setting an optional base context for functions. (#287)
    * Support setting an optional base context for functions.
    
    * refactor: remove exported context related Function methods

--- lambda/entry.go
+++ lambda/entry.go
@@ -3,6 +3,7 @@
 package lambda
 
 import (
+	"context"
 	"log"
 	"net"
 	"net/rpc"
@@ -37,8 +38,12 @@ import (
 // Where "TIn" and "TOut" are types compatible with the "encoding/json" standard library.
 // See https://golang.org/pkg/encoding/json/#Unmarshal for how deserialization behaves
 func Start(handler interface{}) {
-	wrappedHandler := NewHandler(handler)
-	StartHandler(wrappedHandler)
+	StartWithContext(context.Background(), handler)
+}
+
+// StartWithContext is the same as Start except sets the base context for the function.
+func StartWithContext(ctx context.Context, handler interface{}) {
+	StartHandlerWithContext(ctx, NewHandler(handler))
 }
 
 // StartHandler takes in a Handler wrapper interface which can be implemented either by a
@@ -48,15 +53,26 @@ func Start(handler interface{}) {
 //
 //  func Invoke(context.Context, []byte) ([]byte, error)
 func StartHandler(handler Handler) {
+	StartHandlerWithContext(context.Background(), handler)
+}
+
+// StartHandlerWithContext is the same as StartHandler except sets the base context for the function.
+//
+// Handler implementation requires a single "Invoke()" function:
+//
+//  func Invoke(context.Context, []byte) ([]byte, error)
+func StartHandlerWithContext(ctx context.Context, handler Handler) {
 	port := os.Getenv("_LAMBDA_SERVER_PORT")
 	lis, err := net.Listen("tcp", "localhost:"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = rpc.Register(NewFunction(handler))
-	if err != nil {
+
+	fn := NewFunction(handler).withContext(ctx)
+	if err := rpc.Register(fn); err != nil {
 		log.Fatal("failed to register handler function")
 	}
+
 	rpc.Accept(lis)
 	log.Fatal("accept should not have returned")
 }
