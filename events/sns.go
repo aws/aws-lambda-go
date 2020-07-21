e9e6220a4d688b1b5a70c7643c87f0d38fea37f3
Paritosh Walvekar <paritosh.walvekar@gmail.com>
2020-07-21 10:41:27 -0400

    Define CloudWatch Alarm Payload structure (#296)
    * Added CloudWatchAlarmSnsPayload struct and nested structs within the payload
    
    * Added test for the unmarshaling of CloudWatchAlarmSnsPayload
    
    * Added sample event json files
    
    * Adding module files
    
    * Fixed json tag
    
    * Initialisms and acronyms changed to caps.
    
    * Sanitized the example json payload
    
    * Removed unused dependency
    
    * Fix the new type name using caps
    
    * Changed all CloudWatch types to not being pointers for consistency

--- events/sns.go
+++ events/sns.go
@@ -30,3 +30,59 @@ type SNSEntity struct {
 	UnsubscribeURL    string                 `json:"UnsubscribeUrl"`
 	Subject           string                 `json:"Subject"`
 }
+
+type CloudWatchAlarmSNSPayload struct {
+	AlarmName        string                 `json:"AlarmName"`
+	AlarmDescription string                 `json:"AlarmDescription"`
+	AWSAccountID     string                 `json:"AWSAccountId"`
+	NewStateValue    string                 `json:"NewStateValue"`
+	NewStateReason   string                 `json:"NewStateReason"`
+	StateChangeTime  string                 `json:"StateChangeTime"`
+	Region           string                 `json:"Region"`
+	AlarmARN         string                 `json:"AlarmArn"`
+	OldStateValue    string                 `json:"OldStateValue"`
+	Trigger          CloudWatchAlarmTrigger `json:"Trigger"`
+}
+
+type CloudWatchAlarmTrigger struct {
+	Period                           int64                       `json:"Period"`
+	EvaluationPeriods                int64                       `json:"EvaluationPeriods"`
+	ComparisonOperator               string                      `json:"ComparisonOperator"`
+	Threshold                        float64                     `json:"Threshold"`
+	TreatMissingData                 string                      `json:"TreatMissingData"`
+	EvaluateLowSampleCountPercentile string                      `json:"EvaluateLowSampleCountPercentile"`
+	Metrics                          []CloudWatchMetricDataQuery `json:"Metrics,omitempty"`
+	MetricName                       string                      `json:"MetricName,omitempty"`
+	Namespace                        string                      `json:"Namespace,omitempty"`
+	StatisticType                    string                      `json:"StatisticType,omitempty"`
+	Statistic                        string                      `json:"Statistic,omitempty"`
+	Unit                             string                      `json:"Unit,omitempty"`
+	Dimensions                       []CloudWatchDimension       `json:"Dimensions,omitempty"`
+}
+
+type CloudWatchMetricDataQuery struct {
+	Expression string               `json:"Expression,omitempty"`
+	ID         string               `json:"Id"`
+	Label      string               `json:"Label,omitempty"`
+	MetricStat CloudWatchMetricStat `json:"MetricStat,omitempty"`
+	Period     int64                `json:"Period,omitempty"`
+	ReturnData bool                 `json:"ReturnData,omitempty"`
+}
+
+type CloudWatchMetricStat struct {
+	Metric CloudWatchMetric `json:"Metric"`
+	Period int64            `json:"Period"`
+	Stat   string           `json:"Stat"`
+	Unit   string           `json:"Unit,omitempty"`
+}
+
+type CloudWatchMetric struct {
+	Dimensions []CloudWatchDimension `json:"Dimensions,omitempty"`
+	MetricName string                `json:"MetricName,omitempty"`
+	Namespace  string                `json:"Namespace,omitempty"`
+}
+
+type CloudWatchDimension struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/aws/aws-lambda-go
 go 1.12
 
 require (
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.5.1
 	github.com/urfave/cli/v2 v2.1.1
 )
