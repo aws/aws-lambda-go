cabf7e69927a853ccb5be8269c5ef51cdc35fb5b
Krishnaswamy <shask@amazon.com>
2020-12-17 14:24:16 -0800

    Formatted

--- events/activemq.go
+++ events/activemq.go
@@ -3,28 +3,28 @@
 package events
 
 type ActiveMQEvent struct {
-	EventSource    string              `json:"eventSource"`
-	EventSourceArn string              `json:"eventSourceArn"`
-	Messages       []ActiveMQMessage   `json:"messages"`
+	EventSource    string            `json:"eventSource"`
+	EventSourceArn string            `json:"eventSourceArn"`
+	Messages       []ActiveMQMessage `json:"messages"`
 }
 
 type ActiveMQMessage struct {
-	MessageID       string          `json:"messageID"`
-	MessageType     string          `json:"messageType"`
-	Timestamp       int64           `json:"timestamp"`
-	DeliveryMode    int             `json:"deliveryMode"`
-	CorrelationID   string          `json:"correlationID"`
-	ReplyTo         string          `json:"replyTo"`
-	Destination     Destination     `json:"destination"`
-	Redelivered     bool            `json:"redelivered"`
-	Type            string          `json:"type"`
-	Expiration      int64           `json:"expiration"`
-	Priority        int             `json:"priority"`
-	Data            string          `json:"data"`
-	BrokerInTime    int64           `json:"brokerInTime"`
-	BrokerOutTime   int64           `json:"brokerOutTime"`
+	MessageID     string      `json:"messageID"`
+	MessageType   string      `json:"messageType"`
+	Timestamp     int64       `json:"timestamp"`
+	DeliveryMode  int         `json:"deliveryMode"`
+	CorrelationID string      `json:"correlationID"`
+	ReplyTo       string      `json:"replyTo"`
+	Destination   Destination `json:"destination"`
+	Redelivered   bool        `json:"redelivered"`
+	Type          string      `json:"type"`
+	Expiration    int64       `json:"expiration"`
+	Priority      int         `json:"priority"`
+	Data          string      `json:"data"`
+	BrokerInTime  int64       `json:"brokerInTime"`
+	BrokerOutTime int64       `json:"brokerOutTime"`
 }
 
 type Destination struct {
-	PhysicalName     string    `json:"physicalName"`
+	PhysicalName string `json:"physicalName"`
 }
