033c0a923bdf7d578d84a0e08beecb2343b78440
Luke Young <91491244+lyoung-confluent@users.noreply.github.com>
2021-11-15 21:30:42 -0800

    Create streams.go

---
+++ events/streams.go
@@ -0,0 +1,11 @@
+package events
+
+// StreamsEventResponse is the outer structure to report batch item failures for KinesisEvent and DynamoDBEvent.
+type StreamsEventResponse struct {
+	BatchItemFailures []BatchItemFailure `json:"batchItemFailures"`
+}
+
+// Record which failed processing. 
+type BatchItemFailure struct {
+	ItemIdentifier string `json:"itemIdentifier"`	
+}
