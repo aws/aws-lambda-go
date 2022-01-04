8e3f85565c73ee0931a5a4e37f6aadd6919ef16d
Luke Young <91491244+lyoung-confluent@users.noreply.github.com>
2022-01-04 10:27:20 -0800

    Split StreamsEventResponse into duplicate structs

--- events/streams.go
+++ events/streams.go
@@ -1,11 +1,31 @@
 package events
 
-// StreamsEventResponse is the outer structure to report batch item failures for KinesisEvent and DynamoDBEvent.
-type StreamsEventResponse struct {
-	BatchItemFailures []BatchItemFailure `json:"batchItemFailures"`
+// KinesisEventResponse is the outer structure to report batch item failures for KinesisEvent.
+type KinesisEventResponse struct {
+	BatchItemFailures []KinesisBatchItemFailure `json:"batchItemFailures"`
 }
 
-// Record which failed processing.
-type BatchItemFailure struct {
+// KinesisBatchItemFailure is the individual record which failed processing.
+type KinesisBatchItemFailure struct {
+	ItemIdentifier string `json:"itemIdentifier"`
+}
+
+// DynamoDBEventResponse is the outer structure to report batch item failures for DynamoDBEvent.
+type DynamoDBEventResponse struct {
+	BatchItemFailures []DynamoDBBatchItemFailure `json:"batchItemFailures"`
+}
+
+// DynamoDBBatchItemFailure is the individual record which failed processing.
+type DynamoDBBatchItemFailure struct {
+	ItemIdentifier string `json:"itemIdentifier"`
+}
+
+// SQSEventResponse is the outer structure to report batch item failures for SQSEvent.
+type SQSEventResponse struct {
+	BatchItemFailures []SQSBatchItemFailure `json:"batchItemFailures"`
+}
+
+// SQSBatchItemFailure is the individual record which failed processing.
+type SQSBatchItemFailure struct {
 	ItemIdentifier string `json:"itemIdentifier"`
 }
