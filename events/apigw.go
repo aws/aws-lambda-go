fbba6629f7712f5a5f87cda8ebb1899f5313986b
Travis James <74213215+tjames-stig@users.noreply.github.com>
2022-03-28 20:31:18 -0400

    Add APIGatewayv2 Lambda Authorizer support in events (#399)
    * add authorizer structs to apigw.go
    
    * Fix lint failures on "Id"
    
    * Change Context to a map[string]interface{} in IAMPolicyResponse
    
    * Edit apigw.go to resolve linting errors
    
    * Add nolint tags to "Arn" lint failures
    
    * add test data
    
    * add missing files
    
    Co-authored-by: Bryan Moffatt <bmoffatt@users.noreply.github.com>
    Co-authored-by: Bryan Moffatt <moffattb@amazon.com>

--- events/apigw.go
+++ events/apigw.go
@@ -232,6 +232,50 @@ type APIGatewayV2HTTPRequestContextAuthenticationClientCertValidity struct {
 	NotBefore string `json:"notBefore"`
 }
 
+type APIGatewayV2CustomAuthorizerV1RequestTypeRequestContext struct {
+	Path         string                                               `json:"path"`
+	AccountID    string                                               `json:"accountId"`
+	ResourceID   string                                               `json:"resourceId"`
+	Stage        string                                               `json:"stage"`
+	RequestID    string                                               `json:"requestId"`
+	Identity     APIGatewayCustomAuthorizerRequestTypeRequestIdentity `json:"identity"`
+	ResourcePath string                                               `json:"resourcePath"`
+	HTTPMethod   string                                               `json:"httpMethod"`
+	APIID        string                                               `json:"apiId"`
+}
+
+type APIGatewayV2CustomAuthorizerV1Request struct {
+	Version               string                                                  `json:"version"`
+	Type                  string                                                  `json:"type"`
+	MethodArn             string                                                  `json:"methodArn"` //nolint: stylecheck
+	IdentitySource        string                                                  `json:"identitySource"`
+	AuthorizationToken    string                                                  `json:"authorizationToken"`
+	Resource              string                                                  `json:"resource"`
+	Path                  string                                                  `json:"path"`
+	HTTPMethod            string                                                  `json:"httpMethod"`
+	Headers               map[string]string                                       `json:"headers"`
+	QueryStringParameters map[string]string                                       `json:"queryStringParameters"`
+	PathParameters        map[string]string                                       `json:"pathParameters"`
+	StageVariables        map[string]string                                       `json:"stageVariables"`
+	RequestContext        APIGatewayV2CustomAuthorizerV1RequestTypeRequestContext `json:"requestContext"`
+}
+
+type APIGatewayV2CustomAuthorizerV2Request struct {
+	Version               string                         `json:"version"`
+	Type                  string                         `json:"type"`
+	RouteArn              string                         `json:"routeArn"` //nolint: stylecheck
+	IdentitySource        []string                       `json:"identitySource"`
+	RouteKey              string                         `json:"routeKey"`
+	RawPath               string                         `json:"rawPath"`
+	RawQueryString        string                         `json:"rawQueryString"`
+	Cookies               []string                       `json:"cookies"`
+	Headers               map[string]string              `json:"headers"`
+	QueryStringParameters map[string]string              `json:"queryStringParameters"`
+	RequestContext        APIGatewayV2HTTPRequestContext `json:"requestContext"`
+	PathParameters        map[string]string              `json:"pathParameters"`
+	StageVariables        map[string]string              `json:"stageVariables"`
+}
+
 // APIGatewayCustomAuthorizerContext represents the expected format of an API Gateway custom authorizer response.
 // Deprecated. Code should be updated to use the Authorizer map from APIGatewayRequestIdentity. Ex: Authorizer["principalId"]
 type APIGatewayCustomAuthorizerContext struct {
@@ -289,6 +333,12 @@ type APIGatewayCustomAuthorizerResponse struct {
 type APIGatewayV2CustomAuthorizerSimpleResponse struct {
 	IsAuthorized bool                   `json:"isAuthorized"`
 	Context      map[string]interface{} `json:"context,omitempty"`
+}
+
+type APIGatewayV2CustomAuthorizerIAMPolicyResponse struct {
+	PrincipalID    string                           `json:"principalId"`
+	PolicyDocument APIGatewayCustomAuthorizerPolicy `json:"policyDocument"`
+	Context        map[string]interface{}           `json:"context,omitempty"`
 }
 
 // APIGatewayCustomAuthorizerPolicy represents an IAM policy
