d1db4443511ac37e896bb89316e383a5598eafa9
Bryan Moffatt <moffattb@amazon.com>
2022-05-18 16:50:14 -0700

    add some usage examples

--- lambda/handler.go
+++ lambda/handler.go
@@ -28,6 +28,14 @@ type handlerOptions struct {
 type Option func(*handlerOptions)
 
 // WithContext is a HandlerOption that sets the base context for all invocations of the handler.
+//
+// Usage:
+//  lambda.StartWithOptions(
+//   	func (ctx context.Context) (string, error) {
+//   		return ctx.Value("foo"), nil
+//   	},
+//   	lambda.WithContext(context.WithValue(context.Background(), "foo", "bar"))
+//  )
 func WithContext(ctx context.Context) Option {
 	return Option(func(h *handlerOptions) {
 		h.baseContext = ctx
@@ -35,6 +43,14 @@ func WithContext(ctx context.Context) Option {
 }
 
 // WithSetEscapeHTML sets the SetEscapeHTML argument on the underlying json encoder
+//
+// Usage:
+//  lambda.StartWithOptions(
+//  	func () (string, error) {
+//  		return "<html><body>hello!></body></html>", nil
+//  	},
+//  	lambda.WithSetEscapeHTML(true),
+//  )
 func WithSetEscapeHTML(escapeHTML bool) Option {
 	return Option(func(h *handlerOptions) {
 		h.jsonResponseEscapeHTML = escapeHTML
@@ -42,6 +58,14 @@ func WithSetEscapeHTML(escapeHTML bool) Option {
 }
 
 // WithSetIndent sets the SetIndent argument on the underling json encoder
+//
+// Usage:
+//  lambda.StartWithOptions(
+//  	func (event any) (any, error) {
+//  		return event, nil
+//  	},
+//  	lambda.WithSetIndent(">"," "),
+//  )
 func WithSetIndent(prefix, indent string) Option {
 	return Option(func(h *handlerOptions) {
 		h.jsonResponseIndentPrefix = prefix
