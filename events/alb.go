3985733f9f595e0a856a94b041ae50456ae37f88
Bryan Moffatt <moffattb@amazon.com>
2022-07-13 12:18:40 -0700

    Revert "Add `omitempty` to ALBTargetGroupRequest.Body (#408)"
    This reverts commit 14da40f6fad9d5629abe069408b8ec278c36db75.

--- events/alb.go
+++ events/alb.go
@@ -10,7 +10,7 @@ type ALBTargetGroupRequest struct {
 	MultiValueHeaders               map[string][]string          `json:"multiValueHeaders,omitempty"`
 	RequestContext                  ALBTargetGroupRequestContext `json:"requestContext"`
 	IsBase64Encoded                 bool                         `json:"isBase64Encoded"`
-	Body                            string                       `json:"body,omitempty"`
+	Body                            string                       `json:"body"`
 }
 
 // ALBTargetGroupRequestContext contains the information to identify the load balancer invoking the lambda
