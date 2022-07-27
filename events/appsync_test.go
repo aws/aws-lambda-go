151c2c1593825215e85a273edb33a0a42105e12a
Bryan Moffatt <bmoffatt@users.noreply.github.com>
2022-07-27 09:19:05 -0700

    Mark AppSyncResolverTemplate as Deprecated (#460)

--- events/appsync_test.go
+++ events/appsync_test.go
@@ -9,46 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAppSyncResolverTemplate_invoke(t *testing.T) {
-	inputJSON, err := ioutil.ReadFile("./testdata/appsync-invoke.json")
-	if err != nil {
-		t.Errorf("could not open test file. details: %v", err)
-	}
-
-	var inputEvent AppSyncResolverTemplate
-	if err = json.Unmarshal(inputJSON, &inputEvent); err != nil {
-		t.Errorf("could not unmarshal event. details: %v", err)
-	}
-	assert.Equal(t, OperationInvoke, inputEvent.Operation)
-
-	outputJSON, err := json.Marshal(inputEvent)
-	if err != nil {
-		t.Errorf("could not marshal event. details: %v", err)
-	}
-
-	assert.JSONEq(t, string(inputJSON), string(outputJSON))
-}
-
-func TestAppSyncResolverTemplate_batchinvoke(t *testing.T) {
-	inputJSON, err := ioutil.ReadFile("./testdata/appsync-batchinvoke.json")
-	if err != nil {
-		t.Errorf("could not open test file. details: %v", err)
-	}
-
-	var inputEvent AppSyncResolverTemplate
-	if err = json.Unmarshal(inputJSON, &inputEvent); err != nil {
-		t.Errorf("could not unmarshal event. details: %v", err)
-	}
-	assert.Equal(t, OperationBatchInvoke, inputEvent.Operation)
-
-	outputJSON, err := json.Marshal(inputEvent)
-	if err != nil {
-		t.Errorf("could not marshal event. details: %v", err)
-	}
-
-	assert.JSONEq(t, string(inputJSON), string(outputJSON))
-}
-
 func TestAppSyncIdentity_IAM(t *testing.T) {
 	inputJSON, err := ioutil.ReadFile("./testdata/appsync-identity-iam.json")
 	if err != nil {
