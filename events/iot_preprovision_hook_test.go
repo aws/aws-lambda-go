3dedc3285b5533729a20927fbf0fea1f1a167747
Karl Möller <93589605+karl-dau@users.noreply.github.com>
2023-03-02 22:40:32 +0200

    Add IoT Pre-provisioning hook Request and Reponse structs (#483)

---
+++ events/iot_preprovision_hook_test.go
@@ -0,0 +1,63 @@
+package events
+
+import (
+	"encoding/json"
+	"io/ioutil" //nolint: staticcheck
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events/test"
+)
+
+func TestIoTPreProvisionHookRequest(t *testing.T) {
+
+	// read json from file
+	inputJSON, err := ioutil.ReadFile("./testdata/iot-preprovision-hook-request.json")
+	if err != nil {
+		t.Errorf("could not open test file. details: %v", err)
+	}
+
+	// de-serialize into Go object
+	var inputEvent IoTPreProvisionHookRequest
+	if err := json.Unmarshal(inputJSON, &inputEvent); err != nil {
+		t.Errorf("could not unmarshal event. details: %v", err)
+	}
+
+	// serialize to json
+	outputJSON, err := json.Marshal(inputEvent)
+	if err != nil {
+		t.Errorf("could not marshal event. details: %v", err)
+	}
+
+	test.AssertJsonsEqual(t, inputJSON, outputJSON)
+}
+
+func TestIoTPreProvisionHookRequestMalformedJson(t *testing.T) {
+	test.TestMalformedJson(t, IoTPreProvisionHookRequest{})
+}
+
+func TestIoTPreProvisionHookResponseMarshaling(t *testing.T) {
+
+	// read json from file
+	inputJSON, err := ioutil.ReadFile("./testdata/iot-preprovision-hook-response.json")
+	if err != nil {
+		t.Errorf("could not open test file. details: %v", err)
+	}
+
+	// de-serialize into Go object
+	var inputEvent IoTPreProvisionHookResponse
+	if err := json.Unmarshal(inputJSON, &inputEvent); err != nil {
+		t.Errorf("could not unmarshal event. details: %v", err)
+	}
+
+	// serialize to json
+	outputJSON, err := json.Marshal(inputEvent)
+	if err != nil {
+		t.Errorf("could not marshal event. details: %v", err)
+	}
+
+	test.AssertJsonsEqual(t, inputJSON, outputJSON)
+}
+
+func TestIoTPreProvisionHookResponseMalformedJson(t *testing.T) {
+	test.TestMalformedJson(t, IoTPreProvisionHookResponse{})
+}
