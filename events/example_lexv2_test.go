7d8121eabfc1c4b752502edd4df205a201438724
Joyson Lewis <joysl@amazon.com>
2026-01-20 17:12:52 -0500

    feat: add Amazon Lex V2 event support (#518)
    - Add LexV2Event and LexV2Response structures
    - Add support for Kendra integration and sub-slot elicitation
    - Include test fixtures and example usage
    - Add implementation documentation

---
+++ events/example_lexv2_test.go
@@ -0,0 +1,60 @@
+package events_test
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func ExampleLexV2Event() {
+	event := events.LexV2Event{
+		MessageVersion:   "1.0",
+		InvocationSource: "FulfillmentCodeHook",
+		InputMode:        "Text",
+		SessionID:        "session-123",
+		InputTranscript:  "check my balance",
+		Bot: events.LexV2Bot{
+			ID:       "BOTID",
+			Name:     "BankingBot",
+			AliasID:  "ALIASID",
+			LocaleID: "en_US",
+			Version:  "1",
+		},
+		SessionState: events.LexV2SessionState{
+			Intent: &events.LexV2Intent{
+				Name:              "CheckBalance",
+				State:             "ReadyForFulfillment",
+				ConfirmationState: "None",
+				Slots:             map[string]events.LexV2Slot{},
+			},
+		},
+	}
+
+	fmt.Printf("Bot: %s, Intent: %s\n", event.Bot.Name, event.SessionState.Intent.Name)
+	// Output: Bot: BankingBot, Intent: CheckBalance
+}
+
+func ExampleLexV2Response() {
+	response := events.LexV2Response{
+		SessionState: events.LexV2SessionState{
+			DialogAction: &events.LexV2DialogAction{
+				Type: "Close",
+			},
+			Intent: &events.LexV2Intent{
+				Name:              "CheckBalance",
+				State:             "Fulfilled",
+				ConfirmationState: "None",
+				Slots:             map[string]events.LexV2Slot{},
+			},
+		},
+		Messages: []events.LexV2Message{
+			{
+				ContentType: "PlainText",
+				Content:     "Your balance is $1,234.56",
+			},
+		},
+	}
+
+	fmt.Printf("Action: %s, Message: %s\n", response.SessionState.DialogAction.Type, response.Messages[0].Content)
+	// Output: Action: Close, Message: Your balance is $1,234.56
+}
